d14ba4148ed7b08ee09c4d33094cacc0de619383
Amy_dplug <1067542@pdsb.net>
2025-03-03 10:35:49 -0500

    debugg

---
+++ Python_Coding/5.Project_Wholesome_Positivity_Machine-pt_2😇.py
@@ -0,0 +1,16 @@
+myName = input("who are you? ")
+if myName == "Joshua" or myName == "joshua":
+    print("Well Hi")
+else:
+    print("hmm, idk u anyways...")
+achieve = input("What do you want to achieve? ")
+if achieve == "be the best" or achieve == "teach people how to code!":
+    print ("Noice!")
+else:
+    print("hmm, well that's gud")
+scale = int(input("On a scale of 1 - 10, how do you feel today? ( 1:😓, 10:🥰)"))
+if scale <= 10 and scale >= 1:
+    print("That's great!")
+else:
+    print("Hmm, that doesn't seem right...")
+print(" HEy Joshua, word of advice Dont be a d**k and keep ur head or chin or wtv up", "\033m[36", "TODAY, your going to understand why Amy's seatmate is on something!", "\033[0m",  "Anyways, ur also going to teach people how to code :D in the most amazing way, simply by not being a d**k - YOU ROCK! 'ig'")
