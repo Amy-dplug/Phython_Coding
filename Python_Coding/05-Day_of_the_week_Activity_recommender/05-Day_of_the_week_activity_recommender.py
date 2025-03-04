18ccb81acf54f10a42dd5c2d373733f9594d5374
Amy-dplug <1067542@pdsb.net>
2025-03-04 08:57:07 -0500

    Update 05-Day_of_the_week_activity_recommender.py

--- Python_Coding/05-Day_of_the_week_Activity_recommender/05-Day_of_the_week_activity_recommender.py
+++ Python_Coding/05-Day_of_the_week_Activity_recommender/05-Day_of_the_week_activity_recommender.py
@@ -1,3 +1,13 @@
+#-----------------------------------------------------------------------------
+# Name:        Day of the week Activity recommender (Day_of_the_week_aactivity_recommender.py)
+# Purpose:     To tell user if number is even or odd
+#			   work in Github and Pycharm
+#
+# Author:      Amy Egwumba
+# Created:     20-Feb-2025
+# Updated:     03-Mar-2025
+#-----------------------------------------------------------------------------
+
 # Ask the user to enter the current day of the week
 day = input("Enter the current day of the week: ").strip().lower()
 
