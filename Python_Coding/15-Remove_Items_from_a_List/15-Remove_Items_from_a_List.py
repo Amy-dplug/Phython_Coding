5c82e39b1ed472cf7dcf0f06c0d56981782bacf0
Amy_dplug <1067542@pdsb.net>
2025-05-20 09:01:57 -0400

    New assignments workspace added. Added read me spaces for read me files im learning base on.

--- Python_Coding/15-Remove_Items_from_a_List/15-Remove_Items_from_a_List.py
+++ Python_Coding/15-Remove_Items_from_a_List/15-Remove_Items_from_a_List.py
@@ -1,9 +1,10 @@
 #-----------------------------------------------------------------------------
 #Name:        Remove items from a List (Remove_Items_from_a_list.py)
-#Purpose:     Removes an items from a list
+#Purpose:     Removes an item from a list
 #			  Work in GitHub and Pycharm
 #
 #Author:      Amy Egwumba
 #Created:     01-Apr-2025
-#Updated:     01-Apr-2025
+#Updated:     05-Apr-2025
 #-----------------------------------------------------------------------------
+
